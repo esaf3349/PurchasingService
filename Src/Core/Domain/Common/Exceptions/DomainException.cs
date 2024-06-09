a622c80ce28f8cf7ba89ff763924e9ef95d46afd
Esaf <esaf3349@gmail.com>
2024-06-09 22:22:08 +0300

    Added DomainException

---
+++ Src/Core/Domain/Common/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common.Entities;
+
+namespace Domain.Common.Exceptions;
+
+public class DomainException<TEntity> : Exception, IDomainException where TEntity : IAuditableEntity
+{
+    public DomainException(string message) : base($"{typeof(TEntity).Name}: {message}")
+    {
+
+    }
+}
---
+++ Src/Core/Domain/Common/Exceptions/IDomainException.cs
@@ -0,0 +1,3 @@
+﻿namespace Domain.Common.Exceptions;
+
+public interface IDomainException;
