9a3b28a4d80082a766debbeb59ec279c0d487ad6
Esaf <esaf3349@gmail.com>
2024-06-17 20:35:23 +0300

    Added app config

---
+++ Src/Core/Application/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        var currentAssembly = typeof(ServiceCollectionExtensions).Assembly;
+
+        services.AddMediatR(config => config.RegisterServicesFromAssemblies(currentAssembly));
+
+        services.AddAutoMapper(currentAssembly);
+    }
+}
