53381df445025084387ba9e6ec06a31248df2c0d
Esaf <esaf3349@gmail.com>
2024-06-20 19:55:08 +0300

    Added BaseController

---
+++ Src/Presentation/WebApi/Common/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Contracts.Presentation.CurrentUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Common.Controllers;
+
+internal abstract class BaseController : Controller
+{
+    private const string _currentUserSessionName = "CurrentUser";
+
+    private ICurrentUserService _currentUserService => HttpContext.RequestServices.GetRequiredService<ICurrentUserService>();
+
+    private protected IMediator Mediator => HttpContext.RequestServices.GetRequiredService<IMediator>();
+    private protected string? CurrentUserIdentityName;
+
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (context.HttpContext.Session.Keys.Contains(_currentUserSessionName))
+            CurrentUserIdentityName = context.HttpContext.Session.GetString(_currentUserSessionName);
+        else
+        {
+            CurrentUserIdentityName = _currentUserService.IdentityName;
+            context.HttpContext.Session.SetString(_currentUserSessionName, CurrentUserIdentityName ?? string.Empty);
+        }
+
+        await next();
+    }
+}
