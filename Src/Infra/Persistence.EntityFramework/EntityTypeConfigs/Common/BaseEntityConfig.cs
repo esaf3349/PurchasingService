f6e7ae3488d647aed08dfd25ab6ebf968ec9837c
Esaf <esaf3349@gmail.com>
2024-06-21 18:16:45 +0300

    Added EntityTypeConfig for BaseEntity

---
+++ Src/Infra/Persistence.EntityFramework/EntityTypeConfigs/Common/BaseEntityConfig.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityFramework.EntityTypeConfigs.Common;
+
+internal abstract class BaseEntityConfig<TEntity, TEntityId> : IEntityTypeConfiguration<TEntity>
+    where TEntity : BaseEntity<TEntityId>
+    where TEntityId : struct
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+    }
+}
