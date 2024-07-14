dbc9aed4ce1a7ca728418139c730a04f0dd5f011
Esaf <esaf3349@gmail.com>
2024-07-14 21:21:53 +0300

    Get user: application

---
+++ Src/Core/Application/Requests/Users/Get/GetUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Contracts.Infra.Persistence;
+using Application.Exceptions;
+using Domain.Model.Users;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Requests.Users.Get;
+
+public sealed class GetUserHandler : IRequestHandler<GetUserRequest, User>
+{
+    private readonly IUnitOfWork _uow;
+
+    public GetUserHandler(IUnitOfWork uow)
+    {
+        _uow = uow;
+    }
+
+    public async Task<User> Handle(GetUserRequest request, CancellationToken cancellationToken = default)
+    {
+        var user = await _uow.Users.FirstOrDefaultAsync(u => u.Id == request.Id && u.IsActive);
+        if (user == null)
+            throw new NotFoundException($"User {request.Id} doesn't exists");
+
+        return user;
+    }
+}
---
+++ Src/Core/Application/Requests/Users/Get/GetUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Model.Users;
+using MediatR;
+
+namespace Application.Requests.Users.Get;
+
+public sealed record GetUserRequest : IRequest<User>
+{
+    public required Guid Id { get; init; }
+}
